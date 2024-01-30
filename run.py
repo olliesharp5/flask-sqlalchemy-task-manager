439198567b6dfcc548887874fe53cfd411ad63a5
Oliver Sharp <olliesharp5@gmail.com>
2024-01-30 13:44:17 +0000

    Amended file name for filepath

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from taskmanager import app
 
 if __name__ == "__main__":
     app.run(
-        host=os.environ.get("IP", "0.0.0.0"),
-        port=int(os.environ.get("PORT", "5000")),
-        debug=True)
-
+        host=os.environ.get("IP"),
+        port=int(os.environ.get("PORT")),
+        debug=os.environ.get("DEBUG")
+    )
